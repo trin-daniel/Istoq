45a202b9a7afe04f5ac5afb27104fe99bac0ab18
trin-daniel <trindadedanniel63@outlook.com>
2020-10-08 11:26:34 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -13,30 +13,37 @@ const mockRequest: HttpRequest<any> = {
   }
 }
 
+const makeSut = (): any => {
+  const sut = new SignUpController()
+  return {
+    sut
+  }
+}
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockRequest.body, name: undefined } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockRequest.body, email: undefined } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockRequest.body, password: undefined } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 
   test('Should return 400 if no confirmation is provided', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockRequest.body, confirmation: undefined } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('confirmation')))

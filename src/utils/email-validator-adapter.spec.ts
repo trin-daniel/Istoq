c0b97ba50c0daa8798252a85b67b1a828642c2c2
trin-daniel <trindadedanniel63@outlook.com>
2020-10-09 12:21:09 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,6 +3,7 @@ import { internet } from 'faker'
 import validator from 'validator'
 
 const email = internet.email()
+
 jest.mock('validator', () => ({
   isEmail (): boolean {
     return true
@@ -21,5 +22,12 @@ describe('EmailValidator Adapter', () => {
     const sut = new EmailValidatorAdapter()
     const isEmail = sut.isEmail(email)
     expect(isEmail).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isEmail(email)
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
   })
 })

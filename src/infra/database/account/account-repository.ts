e76034bfd242bee28213d7061cd877def684be79
trin-daniel <trindadedanniel63@outlook.com>
2020-10-10 20:31:44 -0300

    feat: ensure AccountRepository returns an account on add with success

---
+++ src/infra/database/account/account-repository.ts
@@ -0,0 +1,20 @@
+import { AddAccountRepository } from '../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../domain/models/account-model'
+import { AddAccountParams } from '../../../domain/use-cases/add-account'
+import { SqlHelper } from '../helpers/sql-helper'
+
+export class AccountRepository implements AddAccountRepository {
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    const { name, email, password } = params
+    const id = `${Date.now()}${Math.random().toString(36).substr(2, 6)}`
+    const created_at = new Date()
+    const updated_at = new Date()
+    await SqlHelper.query(
+      'INSERT INTO accounts (id, name, email, password, created_at, updated_at) VALUES(?,?,?,?,?,?)',
+      [id, name, email, password, created_at, updated_at]
+    )
+
+    const account = await SqlHelper.query('SELECT * FROM accounts WHERE id = (?)', [id])
+    return account[0][0]
+  }
+}

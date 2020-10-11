baabe296e6f17d1b11f822ef1f07cd155729e04c
trin-daniel <trindadedanniel63@outlook.com>
2020-10-10 23:04:51 -0300

    feat: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import { app } from '../config/app'
+import request from 'supertest'
+
+describe('Cors Middleware', () => {
+  test('Should enable cors', async () => {
+    const url = '/test_cors'
+    app.get(url, (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get(url)
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})

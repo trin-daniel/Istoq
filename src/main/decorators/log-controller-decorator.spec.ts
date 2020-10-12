353826a761dace4cd315ca12a954aad8c7ee61eb
trin-daniel <trindadedanniel63@outlook.com>
2020-10-12 12:44:35 -0300

    refactor: move sut creation to a factory helper method

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -2,35 +2,51 @@ import { LogControllerDecorator } from './log-controller-decorator'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { internet } from 'faker'
 
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
 const passwordFreeze = internet.password()
 const mockHttpRequest: HttpRequest<any> = {
-  body: {
-    name: internet.userName(),
-    email: internet.email(),
-    password: passwordFreeze,
-    confirmation: passwordFreeze
-  }
+  body:
+   {
+     name: internet.userName(),
+     email: internet.email(),
+     password: passwordFreeze,
+     confirmation: passwordFreeze
+   }
 }
 
 const mockHttpResponse: HttpResponse<any> = {
   statusCode: 200,
-  body: {
-    name: internet.userName(),
-    email: internet.email()
+  body:
+   {
+     name: internet.userName(),
+     email: internet.email()
+   }
+}
+
+const mockController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (request: HttpRequest<any>): Promise<HttpResponse<any>> {
+      const httpResponse = mockHttpResponse
+      return Promise.resolve(httpResponse)
+    }
   }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = mockController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return { sut, controllerStub }
 }
 
 describe('Log Controller Decorator', () => {
   test('Should call controller handle with correct value', async () => {
-    class ControllerStub implements Controller {
-      async handle (request: HttpRequest<any>): Promise<HttpResponse<any>> {
-        const httpResponse = mockHttpResponse
-        return Promise.resolve(httpResponse)
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = mockHttpRequest
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)

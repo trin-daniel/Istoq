f43fcc41eb8dbcfb81572056574e4704dd169438
trin-daniel <trindadedanniel63@outlook.com>
2020-10-14 11:21:23 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signin/signin-controller.spec.ts
+++ src/presentation/controllers/signin/signin-controller.spec.ts
@@ -3,6 +3,10 @@ import { badRequest } from '../../helpers/http/http-helpers'
 import { MissingParamError } from '../../errors'
 import { internet } from 'faker'
 
+type SutTypes = {
+  sut: SignInController
+}
+
 const mockHttpRequest = {
   body: {
     email: internet.email(),
@@ -10,16 +14,23 @@ const mockHttpRequest = {
   }
 }
 
+const makeSut = (): SutTypes => {
+  const sut = new SignInController()
+  return {
+    sut
+  }
+}
+
 describe('SignIn Controller', () => {
   test('Should return 400 if no e-mail is provided', async () => {
-    const sut = new SignInController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockHttpRequest.body, email: undefined } }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new SignInController()
+    const { sut } = makeSut()
     const httpRequest = { body: { ...mockHttpRequest.body, password: undefined } }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))

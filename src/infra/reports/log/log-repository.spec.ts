92eae711edecf23b1d2c8cb0a1c091ce53da5228
trin-daniel <trindadedanniel63@outlook.com>
2020-10-13 21:29:16 -0300

    test: ensure LogRepository creates an error log on success and create a reports folder

---
+++ src/infra/reports/log/log-repository.spec.ts
@@ -0,0 +1,30 @@
+import {LogRepository} from './log-repository'
+import fs from 'fs'
+
+describe('Log Repository', ()=>{
+  beforeAll(async () =>{
+    await fs.promises.rmdir(`${__dirname}/../../../../reports`, {recursive: true})
+  })
+
+  afterAll(async ()=>{
+    await fs.promises.rmdir(`${__dirname}/../../../../reports/`, {recursive: true})
+  })
+
+  test('Should create report folder if no exist', async ()=>{
+    const sut = new LogRepository()
+    const mkdirSpy = jest.spyOn(fs.promises, 'mkdir')
+    const error = 'any_error'
+    await sut.logError(error)
+    expect(mkdirSpy).toHaveBeenCalled()
+  })
+
+  test('Should create an error log on success', async ()=>{
+    const sut = new LogRepository()
+    const writeSpy = jest.spyOn(fs.promises, 'writeFile')
+    const error = 'any_error'
+    await sut.logError(error)
+    const read = await fs.promises.readdir(`${__dirname}/../../../../reports/`)
+    expect(writeSpy).toHaveBeenCalled()
+    expect(read.length).toBe(1)
+  })
+})

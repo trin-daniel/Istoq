7478c4590b1bf2b343520b64b6e9f28961a0c3a7
trin-daniel <trindadedanniel63@outlook.com>
2020-10-17 12:53:05 -0300

    feat: ensure DbAuthentication calls Encrypter with correct id

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,13 +1,16 @@
 import { Authentication, AuthenticationParams } from '../../../domain/use-cases/authentication'
+import { Encrypter } from '../../protocols/cryptography/encrypter'
 import { HashComparer } from '../../protocols/cryptography/hash-Comparer'
 import { LoadAccountByEmailRepository } from '../../protocols/database/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer) {
+  private readonly encrypter: Encrypter
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, encrypter: Encrypter) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
+    this.encrypter = encrypter
   }
 
   async auth (authentication: AuthenticationParams): Promise<string> {
@@ -15,6 +18,7 @@ export class DbAuthentication implements Authentication {
     const account = await this.loadAccountByEmailRepository.load(email)
     if (account) {
       await this.hashComparer.compare(password, account.password)
+      await this.encrypter.encrypt(account.id)
     }
     return null
   }

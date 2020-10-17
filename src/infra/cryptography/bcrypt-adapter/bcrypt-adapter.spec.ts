1993ed3ccd0bb85e52b698a20bb5ba188541ed88
trin-daniel <trindadedanniel63@outlook.com>
2020-10-17 20:12:39 -0300

    refactor: remove code duplication

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -3,6 +3,7 @@ import { internet, random } from 'faker'
 import bcrypt from 'bcrypt'
 
 const hashedPassword = random.uuid()
+const password = internet.password()
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
@@ -22,21 +23,18 @@ describe('Bcrypt Adapter', () => {
   test('Should call hash with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const password = internet.password()
     await sut.hash(password)
     expect(hashSpy).toHaveBeenCalledWith(password, salt)
   })
 
   test('Should return a valid hash on hash success', async () => {
     const sut = makeSut()
-    const password = internet.password()
     const hash = await sut.hash(password)
     expect(hash).toBe(hashedPassword)
   })
 
   test('Should throw if hash throws', async () => {
     const sut = makeSut()
-    const password = internet.password()
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.hash(password)
     await expect(promise).rejects.toThrow()
@@ -45,7 +43,6 @@ describe('Bcrypt Adapter', () => {
   test('Should call compare with correct values', async () => {
     const sut = makeSut()
     const compareSpy = jest.spyOn(bcrypt, 'compare')
-    const password = internet.password()
     await sut.compare(password, hashedPassword)
     expect(compareSpy).toHaveBeenCalledWith(password, hashedPassword)
   })

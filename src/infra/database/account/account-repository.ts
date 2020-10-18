764813fb11e04adbc887b2b04ed4c0c4169e5da6
trin-daniel <trindadedanniel63@outlook.com>
2020-10-18 13:08:40 -0300

    feat: ensure AccountRepository returns an account on loadByEmail success

--- src/infra/database/account/account-repository.ts
+++ src/infra/database/account/account-repository.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../data/protocols/database/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../data/protocols/database/load-account-by-email-repository'
 import { Account } from '../../../domain/models/account'
 import { AddAccountParams } from '../../../domain/use-cases/add-account'
 import { SqlHelper } from '../helpers/sql-helper'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (params: AddAccountParams): Promise<Account> {
     const { name, email, password } = params
     const id = `${Date.now()}${Math.random().toString(36).substr(2, 6)}`
@@ -15,6 +16,11 @@ export class AccountRepository implements AddAccountRepository {
     )
 
     const account = await SqlHelper.runQuery('SELECT * FROM accounts WHERE id = (?)', [id])
+    return account[0][0]
+  }
+
+  async loadByEmail (email: string): Promise<Account> {
+    const account = await SqlHelper.runQuery('SELECT * FROM accounts WHERE email = (?)', [email])
     return account[0][0]
   }
 }

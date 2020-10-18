64a44d4c899d39ab9700c75f5ac51041f7c6197a
trin-daniel <trindadedanniel63@outlook.com>
2020-10-18 13:08:56 -0300

    test: ensure AccountRepository returns an account on loadByEmail success

--- src/infra/database/account/account-repository.spec.ts
+++ src/infra/database/account/account-repository.spec.ts
@@ -25,13 +25,27 @@ describe('Account Repository', () => {
     await SqlHelper.runQuery('truncate table accounts')
   })
 
-  afterEach(async () => {
-    await SqlHelper.runQuery('truncate table accounts')
-  })
-
   test('Should return an account on add with success', async () => {
     const sut = makeSut()
     const account = await sut.add(params)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(params.name)
+    expect(account.email).toBe(params.email)
+    expect(account.password).toBe(params.password)
+  })
+
+  test('Should return an account on loadByEmail success', async () => {
+    const { name, email, password } = params
+    const id = `${Date.now()}${Math.random().toString(36).substr(2, 6)}`
+    const created_at = new Date()
+    const updated_at = new Date()
+    await SqlHelper.runQuery(
+      'INSERT INTO accounts (id, name, email, password, created_at, updated_at) VALUES(?,?,?,?,?,?)',
+      [id, name, email, password, created_at, updated_at]
+    )
+    const sut = makeSut()
+    const account = await sut.loadByEmail(params.email)
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe(params.name)

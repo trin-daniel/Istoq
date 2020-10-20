dfb33c4f0123acb35b76c65a8a78453f4461beb9
trin-daniel <trindadedanniel63@outlook.com>
2020-10-19 21:36:41 -0300

    test: add Signin route

---
+++ src/main/routes/signin-route.test.ts
@@ -0,0 +1,46 @@
+import { app } from '../config/app'
+import { SqlHelper } from '../../infra/database/helpers/sql-helper'
+import { internet } from 'faker'
+import { hash } from 'bcrypt'
+import request from 'supertest'
+
+describe('SignIn Routes', () => {
+  beforeAll(async () => {
+    await SqlHelper.connect()
+  })
+
+  afterAll(async () => {
+    await SqlHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await SqlHelper.runQuery('truncate table accounts')
+  })
+
+  afterEach(async () => {
+    await SqlHelper.runQuery('truncate table accounts')
+  })
+
+  test('Should return 200 on signin', async () => {
+    const url = '/api/signin'
+    const id = `${Date.now()}${Math.random().toString(36).substr(2, 6)}`
+    const name = internet.userName()
+    const email = internet.email()
+    const password = internet.password()
+    const hashedPassword = await hash(password, 12)
+    const created_at = new Date()
+    const updated_at = new Date()
+    await SqlHelper.runQuery(
+      'INSERT INTO accounts (id, name, email, password, created_at, updated_at) VALUES(?,?,?,?,?,?)',
+      [id, name, email, hashedPassword, created_at, updated_at]
+    )
+    const httpRequest = {
+      email,
+      password
+    }
+    await request(app)
+      .post(url)
+      .send(httpRequest)
+      .expect(200)
+  })
+})

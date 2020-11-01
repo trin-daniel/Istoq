bf083e578bcde09e60c55357b9ec49d195289112
trin-daniel <trindadedanniel63@outlook.com>
2020-11-01 17:53:23 -0300

    test: ensure QuantityValidation returns a InvalidParamError if validation fails

---
+++ src/validation/validators/quantity-validation.spec.ts
@@ -0,0 +1,23 @@
+import { QuantityValidation } from '@validation/validators/quantity-validation'
+import { InvalidParamError } from '@presentation/errors'
+import { random } from 'faker'
+
+type SutTypes = {
+  sut: QuantityValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new QuantityValidation('quantity')
+  return {
+    sut
+  }
+}
+
+describe('Quantity Validation', () => {
+  test('Should return a InvalidParamError if validation fails', async () => {
+    const { sut } = makeSut()
+    const input = { quantity: random.word() }
+    const error = sut.validate(input)
+    expect(error).toEqual(new InvalidParamError('quantity'))
+  })
+})

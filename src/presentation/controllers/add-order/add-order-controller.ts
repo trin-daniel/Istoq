0402476f488d2c45bd09abc221a99f0a0c131496
trin-daniel <trindadedanniel63@outlook.com>
2020-11-01 22:56:05 -0300

    feat: ensure AddOrderController returns a badRequest error if validation fails

--- src/presentation/controllers/add-order/add-order-controller.ts
+++ src/presentation/controllers/add-order/add-order-controller.ts
@@ -1,10 +1,12 @@
 import { Controller, HttpRequest, HttpResponse, Validation } from '@presentation/controllers/add-order/add-order-controller-protocols'
+import { badRequest } from '@presentation/helpers/http/http-helpers'
 
 export class AddOrderController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(request.body)
+    const error = this.validation.validate(request.body)
+    if (error) return badRequest(error)
     return Promise.resolve(null)
   }
 }

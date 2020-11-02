798bb85f95882e7db8cafd36b581c49441a4f71d
trin-daniel <trindadedanniel63@outlook.com>
2020-11-01 22:46:40 -0300

    test: ensure AddOrderController calls Validation with correct values

---
+++ src/presentation/controllers/add-order/add-order-controller.spec.ts
@@ -0,0 +1,47 @@
+import { AddOrderController } from '@presentation/controllers/add-order/add-order-controller'
+import { HttpRequest, Validation } from '@presentation/controllers/add-order/add-order-controller-protocols'
+import { internet, random } from 'faker'
+
+type SutTypes = {
+  sut: AddOrderController
+  validationStub: Validation
+}
+
+const mockRequest: HttpRequest = {
+  account_id: random.uuid(),
+  body: {
+    client: internet.userName(),
+    product: random.word(),
+    quantity: random.number().toString(),
+    discount: random.number(100).toString(),
+    price: random.float().toString()
+  }
+}
+
+const mockValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = mockValidation()
+  const sut = new AddOrderController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('Add Order Controller', () => {
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const request = mockRequest
+    await sut.handle(request)
+    expect(validateSpy).toHaveBeenCalledWith(request.body)
+  })
+})

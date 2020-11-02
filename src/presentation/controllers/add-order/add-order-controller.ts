2c90788dd04dc5fae65b707406f370ef071ce9b5
trin-daniel <trindadedanniel63@outlook.com>
2020-11-01 23:39:39 -0300

    feat: ensure AddOrderController returns 500 if AddOrder throws

--- src/presentation/controllers/add-order/add-order-controller.ts
+++ src/presentation/controllers/add-order/add-order-controller.ts
@@ -1,15 +1,18 @@
 import { AddOrder, Controller, HttpRequest, HttpResponse, Validation } from '@presentation/controllers/add-order/add-order-controller-protocols'
-import { badRequest, ok } from '@presentation/helpers/http/http-helpers'
+import { badRequest, ok, serverError } from '@presentation/helpers/http/http-helpers'
 
 export class AddOrderController implements Controller {
   constructor (private readonly validation: Validation, private readonly addOrder: AddOrder) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(request.body)
-    if (error) return badRequest(error)
-    const { account_id } = request
-
-    const order = await this.addOrder.add({ ...request.body, account_id })
-    return ok(order)
+    try {
+      const { account_id } = request
+      const error = this.validation.validate(request.body)
+      if (error) return badRequest(error)
+      const order = await this.addOrder.add({ ...request.body, account_id })
+      return ok(order)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }

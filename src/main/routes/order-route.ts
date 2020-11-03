2a9457742c51966b9cdc0f8ad914cee9a9d9a908
trin-daniel <trindadedanniel63@outlook.com>
2020-11-03 00:35:51 -0300

    refactor: use middleware authUser

--- src/main/routes/order-route.ts
+++ src/main/routes/order-route.ts
@@ -1,10 +1,8 @@
-import { expressMiddlewareAdapter } from '@main/adapters/express/express-middleware-adapter'
 import { expressRouterAdapter } from '@main/adapters/express/express-route-adapter'
 import { makeAddOrderControllerFactory } from '@main/factories/controllers/add-order/add-order-controller-factory'
-import { makeAuthMiddlewareFactory } from '@main/factories/middlewares/auth-middleware-factory'
+import { authUser } from '@main/middlewares/auth-user'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  const auth = expressMiddlewareAdapter(makeAuthMiddlewareFactory())
-  router.post('/orders', auth, expressRouterAdapter(makeAddOrderControllerFactory()))
+  router.post('/orders', authUser, expressRouterAdapter(makeAddOrderControllerFactory()))
 }

0aeed2194864e14570b14dd3d868cd54642edf4a
trin-daniel <trindadedanniel63@outlook.com>
2020-11-03 00:36:38 -0300

    test: ensure order route returns 200 if token is provided

--- src/main/routes/order-route.test.ts
+++ src/main/routes/order-route.test.ts
@@ -1,7 +1,9 @@
 import { SqlHelper } from '@infra/database/helpers'
 import { app } from '@main/config/app'
 import { HttpRequest } from '@presentation/protocols'
+import { hash } from 'bcrypt'
 import { internet, random } from 'faker'
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 
 const mockRequest: HttpRequest = {
@@ -24,5 +26,25 @@ describe('Order Route', () => {
       .post('/api/orders')
       .send(mockRequest)
       .expect(403)
+  })
+
+  test('Should return 200 if token is provided', async () => {
+    const id = random.uuid()
+    const token = sign({ id }, 'secret', { expiresIn: '1d' })
+    const name = internet.userName()
+    const email = internet.email()
+    const password = internet.password()
+    const hashed = await hash(password, 12)
+    const created_at = new Date()
+    const updated_at = new Date()
+    await SqlHelper.runQuery(
+      'INSERT INTO accounts (id, token, name, email, password, created_at, updated_at) VALUES(?,?,?,?,?,?,?)',
+      [id, token, name, email, hashed, created_at, updated_at]
+    )
+    await request(app)
+      .post('/api/orders')
+      .set('x-access-token', token)
+      .send(mockRequest.body)
+      .expect(200)
   })
 })
